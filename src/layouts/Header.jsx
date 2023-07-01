a2a01f6cb8bbed32fc36b26668f67c8fa6936a98
Olumide Michael <olumide.m.ojo@gmail.com>
2023-07-01 18:57:18 +0100

    feat: added navlink component

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import styles from '../layouts/layout.module.css';
 import Hamburger from '../components/hamburger/hamburger';
 import Logo from '../components/logo/Logo';
+import Navlink from '../components/navlink/Navlink';
 
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.logoContainer}><Logo/></div>
-      <div className={styles.navlinkContainer}>NavLink</div>
+      <div className={styles.navlinkContainer}><Navlink/></div>
       <div className={styles.hamburgerContainer}><Hamburger/></div>
     </header>
   )
