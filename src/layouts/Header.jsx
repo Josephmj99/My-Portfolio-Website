bc4038bb944f1a428d96cb1875a664b5f0e9054c
Olumide Michael <olumide.m.ojo@gmail.com>
2023-07-01 17:03:11 +0100

    feat: added logo

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from '../layouts/layout.module.css';
 import Hamburger from '../components/hamburger/hamburger';
+import Logo from '../components/logo/Logo';
 
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.logoContainer}>Logo</div>
+      <div className={styles.logoContainer}><Logo/></div>
       <div className={styles.navlinkContainer}>NavLink</div>
       <div className={styles.hamburgerContainer}><Hamburger/></div>
     </header>
